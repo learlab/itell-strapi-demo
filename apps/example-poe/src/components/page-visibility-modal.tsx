3620233655c430d37c9795db3d6d651e79059519
theannachen <annachen031014@gmail.com>
2023-11-30 16:48:49 -0600

    Update page-visibility-modal.tsx

--- apps/example-poe/src/components/page-visibility-modal.tsx
+++ apps/example-poe/src/components/page-visibility-modal.tsx
@@ -28,38 +28,38 @@ export const PageVisibilityModal = () => {
 
 	if (status === undefined || status === "unlocked") return null;
 
-	if (status === "unauthorizedf") {
-		return (
-			<Dialog
-				open={open}
-				onOpenChange={() => {
-					if (process.env.NODE_ENV === "development") {
-						setOpen(false);
-					}
-				}}
-			>
-				<DialogContent canClose={false}>
-					<DialogHeader>
-						<DialogTitle>Login in to view the textbook</DialogTitle>
-						<DialogDescription>
-							<Collapsible>
-								<CollapsibleTrigger className="m-0 p-0">
-									Why do I need to have an account?
-								</CollapsibleTrigger>
-								<CollapsibleContent>
-									We collects anonymous data to improve learning experience. See{" "}
-									<span className="underline">here</span> for more details.
-								</CollapsibleContent>
-							</Collapsible>
-						</DialogDescription>
-					</DialogHeader>
-					<div className="mt-6">
-						<GoogleLoginButton />
-					</div>
-				</DialogContent>
-			</Dialog>
-		);
-	}
+	// if (status == "unauthorizedf") {
+	// 	return (
+	// 		<Dialog
+	// 			open={open}
+	// 			onOpenChange={() => {
+	// 				if (process.env.NODE_ENV === "development") {
+	// 					setOpen(false);
+	// 				}
+	// 			}}
+	// 		>
+	// 			<DialogContent canClose={false}>
+	// 				<DialogHeader>
+	// 					<DialogTitle>Login in to view the textbook</DialogTitle>
+	// 					<DialogDescription>
+	// 						<Collapsible>
+	// 							<CollapsibleTrigger className="m-0 p-0">
+	// 								Why do I need to have an account?
+	// 							</CollapsibleTrigger>
+	// 							<CollapsibleContent>
+	// 								We collects anonymous data to improve learning experience. See{" "}
+	// 								<span className="underline">here</span> for more details.
+	// 							</CollapsibleContent>
+	// 						</Collapsible>
+	// 					</DialogDescription>
+	// 				</DialogHeader>
+	// 				<div className="mt-6">
+	// 					<GoogleLoginButton />
+	// 				</div>
+	// 			</DialogContent>
+	// 		</Dialog>
+	// 	);
+	// }
 
 	if (status === "locked" && userLocation) {
 		const href = makeLocationHref(userLocation);
