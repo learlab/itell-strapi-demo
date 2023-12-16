acab1a1cb0befa0cb9714b9c18ca45086d478a21
theannachen <annachen031014@gmail.com>
2023-12-15 20:29:45 -0600

    more copy and paste

--- apps/example-poe/src/components/provider/page-provider.tsx
+++ apps/example-poe/src/components/provider/page-provider.tsx
@@ -1,17 +1,7 @@
 "use client";
 
-import { PythonProvider as WebpyProvider } from "@webpy/react";
 import { QAProvider } from "../context/qa-context";
 
-const pythonSetupCode = `
-import io
-import contextlib
-`;
-
 export const PageProvider = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<WebpyProvider options={{ setUpCode: pythonSetupCode }}>
-			<QAProvider>{children}</QAProvider>
-		</WebpyProvider>
-	);
+	return <QAProvider>{children}</QAProvider>;
 };
