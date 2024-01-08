4192fca230923254c239adf395a46c77e97dd713
Qiushi Yan <qiushi.yann@gmail.com>
2024-01-08 11:18:06 -0600

    debug user progress

--- apps/example-poe/src/components/dashboard/user/user-progress.tsx
+++ apps/example-poe/src/components/dashboard/user/user-progress.tsx
@@ -4,16 +4,16 @@ import { PageData, getPageData } from "@/lib/utils";
 import { User } from "@prisma/client";
 
 export const UserProgress = ({ user }: { user: User }) => {
-	const isBlankUser = !user.pageSlug;
-	const usersIndex = (getPageData(user.pageSlug) as PageData).index;
-	const pagesUnlocked = isBlankUser ? 0 : usersIndex;
+	const pagesUnlocked = user.pageSlug
+		? getPageData(user.pageSlug)?.index || 0
+		: 0;
 	const progress = (pagesUnlocked / allPagesSorted.length) * 100;
 	return (
 		<div className="flex items-center gap-4">
 			<Progress value={progress} className="w-1/3" />
 			<p className="text-muted-foreground">
-				{progress.toFixed(2)}% completed, {usersIndex}/{allPagesSorted.length}{" "}
-				sections
+				{progress.toFixed(2)}% completed, {pagesUnlocked}/
+				{allPagesSorted.length} sections
 			</p>
 		</div>
 	);
