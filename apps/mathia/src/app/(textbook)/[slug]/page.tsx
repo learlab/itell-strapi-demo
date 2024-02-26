3fd5f66b010bf73d453eac2b00a68add07ec2d77
theannachen <annachen031014@gmail.com>
2024-02-26 15:45:25 -0600

    Update page.tsx

--- apps/mathia/src/app/(textbook)/[slug]/page.tsx
+++ apps/mathia/src/app/(textbook)/[slug]/page.tsx
@@ -1,95 +1,34 @@
-//@ts-nocheck
-import { Button, Pager } from "@/components/client-components";
-import { ModuleSidebar } from "@/components/module-sidebar";
+import { ChatbotLoader } from "@/components/chat/chatbot-loader";
+import { Pager } from "@/components/client-components";
+import { ConstructedResponseControl } from "@/components/constructed-response/constructed-response-control";
+import { ModuleToc } from "@/components/module-toc";
 import { NoteCount } from "@/components/note/note-count";
-import { NoteList } from "@/components/note/note-list";
+import { NoteLoader } from "@/components/note/note-loader";
 import { NoteToolbar } from "@/components/note/note-toolbar";
 import { PageStatus } from "@/components/page-status/page-status";
 import { PageStatusModal } from "@/components/page-status/page-status-modal";
 import { PageTitle } from "@/components/page-title";
 import { PageToc } from "@/components/page-toc";
 import { PageContent } from "@/components/page/page-content";
-import { RestartPageButton } from "@/components/page/restart-page-button";
 import { PageProvider } from "@/components/provider/page-provider";
-import { QuestionControl } from "@/components/question/question-control";
 import { Spinner } from "@/components/spinner";
 import { PageSummary } from "@/components/summary/page-summary";
 import { EventTracker } from "@/components/telemetry/event-tracker";
 import { getCurrentUser } from "@/lib/auth";
 import { getPageChunks } from "@/lib/chunks";
-import { isProduction } from "@/lib/constants";
 import { isPageWithFeedback } from "@/lib/feedback";
 import { getPageStatus } from "@/lib/page-status";
 import { getPagerLinks } from "@/lib/pager";
 import { allPagesSorted } from "@/lib/pages";
 import { getRandomPageQuestions } from "@/lib/question";
-import { getModuleChapters } from "@/lib/sidebar";
 import { getUser } from "@/lib/user";
-import { Page } from "contentlayer/generated";
-import {
-	ArrowUpIcon,
-	EyeIcon,
-	LockIcon,
-	PencilIcon,
-	UnlockIcon,
-} from "lucide-react";
+import { EyeIcon, LockIcon, UnlockIcon } from "lucide-react";
 import { notFound } from "next/navigation";
-import { Fragment, Suspense } from "react";
-
-const AnchorLink = ({
-	text,
-	href,
-	icon,
-}: { text: string; href: string; icon: React.ReactNode }) => {
-	return (
-		<a href={href} className="block">
-			<Button
-				variant="ghost"
-				className="flex flex-wrap justify-start items-center gap-2 pl-0"
-			>
-				{icon}
-				{text}
-			</Button>
-		</a>
-	);
-};
-
-export const LeftAside = ({ page }: { page: Page }) => {
-	const chapters = getModuleChapters(page.location.module);
-
-	return (
-		<aside className="module-sidebar col-span-2 sticky top-20 h-fit">
-			<ModuleSidebar
-				chapters={chapters}
-				currentPage={{
-					chapter: page.location.chapter,
-					section: page.location.section,
-					url: page.url,
-				}}
-			/>
-			<div className="mt-12 space-y-2">
-				<RestartPageButton pageSlug={page.page_slug} />
-				{page.summary && (
-					<AnchorLink
-						icon={<PencilIcon className="size-4" />}
-						text="Write a summary"
-						href="#page-summary"
-					/>
-				)}
-				<AnchorLink
-					icon={<ArrowUpIcon className="size-4" />}
-					text="Back to top"
-					href="#page-title"
-				/>
-			</div>
-		</aside>
-	);
-};
+import { Suspense } from "react";
 
 export default async function ({ params }: { params: { slug: string } }) {
 	const sessionUser = await getCurrentUser();
 	const user = sessionUser ? await getUser(sessionUser.id) : null;
-
 	const pageIndex = allPagesSorted.findIndex((section) => {
 		return section.page_slug === params.slug;
 	});
@@ -126,10 +65,18 @@ export default async function ({ params }: { params: { slug: string } }) {
 			pageStatus={pageStatus}
 			isLastChunkWithQuestion={isLastChunkWithQuestion}
 		>
-			<div className="max-w-[1440px] mx-auto grid grid-cols-12 gap-6 px-2">
-				<LeftAside page={page} />
+			<div className="flex flex-row justify-end max-w-[1440px] mx-auto gap-6 px-2">
+				<aside
+					className="module-sidebar sticky top-20 h-fit z-20 basis-0 animate-out ease-in-out duration-200"
+					style={{ flexGrow: 1 }}
+				>
+					<ModuleToc page={page} />
+				</aside>
 
-				<section className="page-content relative col-span-8">
+				<section
+					className="page-content relative max-w-[850px]"
+					style={{ flexGrow: 4 }}
+				>
 					<PageTitle>
 						{page.title}
 						{isPageLatest ? (
@@ -145,23 +92,28 @@ export default async function ({ params }: { params: { slug: string } }) {
 					<Pager prev={pagerLinks.prev} next={pagerLinks.next} />
 				</section>
 
-				<aside className="toc-sidebar col-span-2 relative">
+				<aside
+					className="toc-sidebar relative animate-out ease-in-out duration-200"
+					style={{ flexGrow: 1 }}
+				>
 					<div className="sticky top-20">
 						<PageToc headings={page.headings} />
 						<div className="mt-8 flex flex-col gap-1">
 							<PageStatus status={pageStatus} />
-							{user && <NoteCount user={user} pageSlug={pageSlug} />}
+							<Suspense fallback={<NoteCount.Skeleton />}>
+								<NoteCount user={sessionUser} pageSlug={pageSlug} />
+							</Suspense>
 						</div>
 					</div>
 					<Suspense
 						fallback={
 							<p className="text-sm text-muted-foreground mt-8">
 								<Spinner className="inline mr-2" />
-								loading notes
+								Loading notes
 							</p>
 						}
 					>
-						<NoteList pageSlug={pageSlug} />
+						<NoteLoader pageSlug={pageSlug} />
 					</Suspense>
 				</aside>
 			</div>
@@ -170,18 +122,22 @@ export default async function ({ params }: { params: { slug: string } }) {
 				<footer>
 					<PageSummary
 						pageSlug={pageSlug}
+						pageStatus={pageStatus}
 						isFeedbackEnabled={isFeedbackEnabled}
 					/>
 				</footer>
 			)}
 
 			<PageStatusModal user={user} pageStatus={pageStatus} />
-			<QuestionControl
+			<ConstructedResponseControl
 				selectedQuestions={selectedQuestions}
 				pageSlug={pageSlug}
 				isFeedbackEnabled={isFeedbackEnabled}
 			/>
-			{user && <EventTracker />}
+			{user && <EventTracker pageSlug={pageSlug} chunks={chunks} />}
+			<Suspense fallback={<ChatbotLoader.Skeleton />}>
+				<ChatbotLoader pageSlug={pageSlug} />
+			</Suspense>
 		</PageProvider>
 	);
 }
