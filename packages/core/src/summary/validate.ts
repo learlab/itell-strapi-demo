55626cceed7c6f85ac4a7df0056a7c90d90c8a70
Qiushi Yan <qiushi.yann@gmail.com>
2024-03-25 17:31:04 -0500

    Update validate.ts

--- packages/core/src/summary/validate.ts
+++ packages/core/src/summary/validate.ts
@@ -13,7 +13,7 @@ export const ErrorFeedback: Record<ErrorType, string> = {
 	[ErrorType.LANGUAGE_NOT_EN]: "Please use English for your summary.",
 	[ErrorType.WORD_COUNT]: "Your summary must be between 50 and 200 words.",
 	[ErrorType.OFFENSIVE]:
-		"Your summary includes inoffensive language. Please remove the offensive language and resubmit.",
+		"Your summary includes offensive language. Please remove the offensive language and resubmit.",
 	[ErrorType.INTERNAL]:
 		"Internal error occurred, please try again later. Contact lear.lab.vu@gmail.com if the problem persists.",
 	[ErrorType.DUPLICATE]: "Please submit a different summary.",
