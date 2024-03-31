096ddecbad05b4dbb505c41097ab56d2d0333289
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2024-03-31 15:14:30 -0500

    Changed Sentry to not report dev bugs

--- apps/cornell/sentry.client.config.ts
+++ apps/cornell/sentry.client.config.ts
@@ -1,11 +1,11 @@
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// https://docs.sentry/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://8d7e4645dca4ef9d1f6f329e29cd9ac5@o4506435193405440.ingest.us.sentry.io/4507007371182080",
+  dsn: "https://8d7e4645dca4ef9d1f6f329e29cd9ac5@o4506435193405440.ingest.us.sentry/4507007371182080",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
@@ -27,4 +27,6 @@ Sentry.init({
       blockAllMedia: true,
     }),
   ],
+
+  enabled: process.env.NODE_ENV !== "development",
 });
