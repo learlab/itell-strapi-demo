ff55de59259084927545cbe1543818e9abeed845
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2024-09-02 16:10:24 -0500

    Checks if is mobile and adds flag

--- packages/core/src/hooks/use-keystroke.ts
+++ packages/core/src/hooks/use-keystroke.ts
@@ -3,43 +3,46 @@
 import { useCallback, useEffect, useRef } from "react";
 
 // keycode, cursor position, downtime, uptime
-type KeystrokeEvent = [string, number, number, number];
+type KeystrokeEvent = [string, number, number, number, boolean];
 
 export const useKeystroke = () => {
-	const ref = useRef<HTMLElement>(null);
-	const keys = useRef<Map<string, number>>(new Map());
-	const data = useRef<KeystrokeEvent[]>([]);
+  const ref = useRef<HTMLElement>(null);
+  const keys = useRef<Map<string, number>>(new Map());
+  const data = useRef<KeystrokeEvent[]>([]);
 
-	const handleKeydown = (e: KeyboardEvent) => {
-		keys.current.set(e.code, e.timeStamp);
-	};
+  const isMobileDevice = /Mobi/i.test(window.navigator.userAgent);
 
-	const handleKeyup = (e: KeyboardEvent) => {
-		const keyDowntime = keys.current.get(e.code);
-		if (keyDowntime) {
-			const d = [
-				e.code,
-				(ref.current as HTMLTextAreaElement).selectionStart,
-				keyDowntime,
-				e.timeStamp,
-			] as KeystrokeEvent;
-			data.current.push(d);
-		}
-	};
+  const handleKeydown = (e: KeyboardEvent) => {
+    keys.current.set(e.code, e.timeStamp);
+  };
 
-	const clear = useCallback(() => {
-		data.current = [];
-	}, []);
+  const handleKeyup = (e: KeyboardEvent) => {
+    const keyDowntime = keys.current.get(e.code);
+    if (keyDowntime) {
+      const d = [
+        e.code,
+        (ref.current as HTMLTextAreaElement).selectionStart,
+        keyDowntime,
+        e.timeStamp,
+        isMobileDevice,
+      ] as KeystrokeEvent;
+      data.current.push(d);
+    }
+  };
 
-	useEffect(() => {
-		ref.current?.addEventListener("keydown", handleKeydown);
-		ref.current?.addEventListener("keyup", handleKeyup);
+  const clear = useCallback(() => {
+    data.current = [];
+  }, []);
 
-		return () => {
-			ref.current?.removeEventListener("keydown", handleKeydown);
-			ref.current?.removeEventListener("keyup", handleKeyup);
-		};
-	}, []);
+  useEffect(() => {
+    ref.current?.addEventListener("keydown", handleKeydown);
+    ref.current?.addEventListener("keyup", handleKeyup);
 
-	return { ref, data: data.current, clear };
+    return () => {
+      ref.current?.removeEventListener("keydown", handleKeydown);
+      ref.current?.removeEventListener("keyup", handleKeyup);
+    };
+  }, []);
+
+  return { ref, data: data.current, clear };
 };
