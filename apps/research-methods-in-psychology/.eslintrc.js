4ddc45d3c21d1c0a4f9cc206557500fb57f152ce
Qiushi Yan <qiushi.yann@gmail.com>
2024-10-21 01:23:26 -0500

    add drizzle eslint plugin

--- apps/research-methods-in-psychology/.eslintrc.js
+++ apps/research-methods-in-psychology/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: ["@itell/eslint-config/next.js"],
+  extends: ["@itell/eslint-config/next.js", "plugin:drizzle/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
 };
