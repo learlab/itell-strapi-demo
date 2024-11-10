9eb94ff32a95f23b5939fa6672da54fcfc48d7be
Joon Suh Choi <57017670+jchoi92k@users.noreply.github.com>
2024-11-10 09:26:02 -0600

    Update summary-streak.ts

--- apps/chevron/src/lib/summary-streak.ts
+++ apps/chevron/src/lib/summary-streak.ts
@@ -3,15 +3,22 @@ import { PersonalizationData } from "@/drizzle/schema";
 export function handleSummaryStreak(personalizationData: PersonalizationData, isPassed: boolean): PersonalizationData {
   personalizationData = personalizationData || {};
 
-  personalizationData.skip_summary_number = isPassed ? 1 : 0;
-
+  // increment streak count by one if summary is a passing one
   let newSummaryStreak = isPassed ? (personalizationData.summary_streak || 0) + 1 : 0;
 
+  // update max streak count
   if (newSummaryStreak > (personalizationData.max_summary_streak || 0)) {
     personalizationData.max_summary_streak = newSummaryStreak;
   }
 
   personalizationData.summary_streak = newSummaryStreak;
+
+  // every new passing summary after a streak allows user to skip one summary
+  if (isPassed && newSummaryStreak >= 2) {
+    personalizationData.skip_summary_number = 1;
+  } else {
+    personalizationData.skip_summary_number = 0;
+  }
 
   return personalizationData;
 }
