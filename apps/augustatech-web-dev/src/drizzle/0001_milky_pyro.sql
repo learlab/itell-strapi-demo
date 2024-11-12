70b4f14d96874a2fdca5b55c9c05be55305c24c1
Qiushi Yan <qiushi.yann@gmail.com>
2024-11-12 11:16:07 -0600

    remove legacy contents

--- apps/augustatech-web-dev/src/drizzle/0001_milky_pyro.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "preferences" jsonb;--> statement-breakpoint
-ALTER TABLE "notes" DROP COLUMN IF EXISTS "y";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN IF EXISTS "time_zone";
