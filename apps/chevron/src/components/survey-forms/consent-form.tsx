b23e7d0fbcb6215412b03b4442264a455f5147ef
Wesley Morris <tiedaar@gmail.com>
2024-11-20 15:48:25 -0600

    Update and connect consent form

--- apps/chevron/src/components/survey-forms/consent-form.tsx
+++ apps/chevron/src/components/survey-forms/consent-form.tsx
@@ -2,26 +2,40 @@
 import React, { useState } from "react";
 import { useRouter } from 'next/navigation';
 import { toast } from "sonner"
+import { Button } from "@itell/ui/button";
+import { Input } from "@itell/ui/input";
+import { useServerAction } from "zsa-react";
+import { updateUserAction } from "@/actions/user";
+import { useEffect } from "react";
 
 const ConsentForm: React.FC = () => {
-    const [consentGiven, setConsentGiven] = useState(false);
+    const [consent, setConsent] = useState(true);
     const router = useRouter();
-
-  
+    const { isPending, execute, isError, error } =
+      useServerAction(updateUserAction);
     const handleConsentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setConsentGiven(event.target.checked);
+      setConsent(event.target.value==='true');
     };
-  
-    const handleSubmit = (event: React.FormEvent) => {
-      
+    
+    const handleSubmit = async (event: React.FormEvent) => {
       event.preventDefault();
-      if (consentGiven) {
+      const [_, err] = await execute({ consent: consent });
+      if (consent) {
         toast.success("Consent given. Thank you!");
-        router.push("/intake1")
+        router.push("/intake1");
       } else {
         toast.warning("Consent not given. You will be redirected to the text anyway.");
+        router.push("/")
       }
     };
+
+    useEffect(() => {
+      if (isError) {
+        toast.error("Error with consent.");
+        reportSentry("consent", { error });
+      }
+    }, [isError]);
+    
   
     return (
       <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-6">
@@ -90,31 +104,35 @@ const ConsentForm: React.FC = () => {
   
           <h2 className="mt-4 text-xl font-semibold text-gray-800">Agreement</h2>
           <p className="mb-4 text-gray-700">
-            If ou are over 18 years old and agree to have your data used for this study, 
+            If you are over 18 years old and agree to have your data used for this study, 
             please indicate your agreement by clicking "I am over 18 years of age and I agree to having my data used in this study"
           </p>        
   
-          <form onSubmit={handleSubmit} className="mt-6">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                id="consent"
-                checked={consentGiven}
-                onChange={handleConsentChange}
-                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-  
-              />
-              <label htmlFor="consent" className="ml-2 text-sm text-gray-700">
-                I have read and understood the information above, I am 18 years or older, and I agree to participate in this study.
-              </label>
+          <form onSubmit={handleSubmit} className="mt-6"> 
+            <div className="flex flex-col p-2 pl-12 gap-3">
+              <Input 
+                type="radio" 
+                id="consentYes"
+                name="consentButtons" 
+                value="true"
+                onChange={handleConsentChange}/>
+              <label htmlFor="html">I have read and understood the information above, I am 18 years or older, and I agree to participate in this study.</label>
+              <Input 
+                type="radio" 
+                id="consentNo"
+                name="consentButtons" 
+                value="false"
+                onChange={handleConsentChange}/>
+              <label htmlFor="css" className="flex center">I am under 18 and/or I do not agree to participate in the study.</label> 
             </div>
   
-            <button
+            <Button
               type="submit"
               className="mt-6 w-full rounded-md bg-indigo-600 py-2 px-4 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              disabled={ isPending }
             >
               Submit Consent Form
-            </button>
+            </Button>
           </form>
         </div>
       </div>
